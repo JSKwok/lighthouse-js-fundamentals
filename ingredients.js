720ef153aa06fd04e1ea3f25ed8f32041277bf52
Justin <justin.s.kwok@gmail.com>
2019-04-17 03:28:07 +0000

    Ingredients exercise

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var count = 0;
+while (count < ingredients.length) {
+  console.log(ingredients[count]);
+  count++;
+}
+
+for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[7 - i]);
+}
